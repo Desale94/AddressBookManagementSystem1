9c672bcca352cbdebe9862ef9307faacdb2f236c
Desale1 <dkdesale08@gmail.com>
2023-02-04 09:39:32 +0530

    Add metthod is call

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,5 +3,7 @@ package com.bridgelabz.address;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program!!");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact();
     }
 }
