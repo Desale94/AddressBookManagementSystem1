f8b3eb0b4f790abe2699a79b47350cc8bb1752fb
Desale1 <dkdesale08@gmail.com>
2023-02-04 09:57:02 +0530

    Create address book for contact

---
+++ AddressBook.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.address;
+import java.util.ArrayList;
+import java.util.List;
+public class AddressBook {
+    Contact person=new Contact();
+    public List<Contact> people = new ArrayList<Contact>();
+}
